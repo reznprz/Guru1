867ff15b58cd74ef1af6d9997a63c012bd3afa2f
rijanprajapati <rijan07mu@gamil.com>
2019-08-27 23:30:16 -0400

    first commit

---
+++ src/main/java/com/example/guru/guru1/Guru1Application.java
@@ -0,0 +1,13 @@
+package com.example.guru.guru1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Guru1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Guru1Application.class, args);
+	}
+
+}
