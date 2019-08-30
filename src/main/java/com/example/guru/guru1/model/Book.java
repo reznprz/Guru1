27cea5c132aa796c85c46c99c6248d9c274a4298
rijanprajapati <rijan07mu@gamil.com>
2019-08-30 00:28:28 -0400

    after adding DevBootStrap and OnetoOne publiser

--- src/main/java/com/example/guru/guru1/model/Book.java
+++ src/main/java/com/example/guru/guru1/model/Book.java
@@ -14,7 +14,9 @@ public class Book {
 
     private String title;
     private String isbn;
-    private String publisher;
+
+    @OneToOne
+    private Publisher publisher;
 
     @ManyToMany
     @JoinTable(name = "authors_book", joinColumns = @JoinColumn(name="book_id"),
@@ -22,13 +24,13 @@ public class Book {
     public Set<Author> authors = new HashSet<>();
 
 
-    public Book(String title, String isbn, String publisher) {
+    public Book(String title, String isbn, Publisher publisher) {
         this.title = title;
         this.isbn = isbn;
         this.publisher = publisher;
     }
 
-    public Book(String title, String isbn, String publisher, Set<Author> authors) {
+    public Book(String title, String isbn, Publisher publisher, Set<Author> authors) {
         this.title = title;
         this.isbn = isbn;
         this.publisher = publisher;
@@ -63,11 +65,11 @@ public class Book {
         this.isbn = isbn;
     }
 
-    public String getPublisher() {
+    public Publisher getPublisher() {
         return publisher;
     }
 
-    public void setPublisher(String publisher) {
+    public void setPublisher(Publisher publisher) {
         this.publisher = publisher;
     }
 
